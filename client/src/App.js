a088cdcaf1845644353345eb1a7d8d91d21a0f07
Prashik <gamreprashik03@gmail.com>
2023-06-19 18:00:57 +0530

    fetch itemdetails

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import Home from "./scenes/home/Home";
 import ItemDetails from "./scenes/itemDetails/ItemDetails";
--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -27,7 +27,8 @@ export default function ShoppingList() {
 
   useEffect(() => {
     getItems();
-  },[])
+  },[]) // eslint-disable-line react-hooks/exhaustive-deps
+
 
   const topRatedItems = items.filter(
     (item) => item.attributes.category === 'topRated'
